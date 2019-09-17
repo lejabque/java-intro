36347ee6c74fd8f04c8d2bf700c979ea45373875
lejabque <vorkdenis1@gmail.com>
2019-09-17 17:14:00 +0200

    SumHex mod

--- hw2/SumHex.java
+++ hw2/SumHex.java
@@ -1,17 +1,27 @@
-public class Sum {
+public class SumHex {
     public static void main(String[] args) {
         int sumResult = 0;
         for (int i = 0; i < args.length; i++) {
-            String s = args[i];
+            String s = args[i].toLowerCase();
             int beginIndex = 0;
             int endIndex = 0;
             while (beginIndex < s.length()) {
                 if (!Character.isWhitespace(s.charAt(beginIndex))) {
+                    boolean hex = false;
+                    if (s.charAt(beginIndex) == '0' && beginIndex + 1 < s.length() && s.charAt(beginIndex + 1) == 'x') {
+                        hex = true;
+                        beginIndex += 2;
+                    }
                     endIndex = beginIndex;
                     while (endIndex < s.length() && !Character.isWhitespace(s.charAt(endIndex))) {
                         endIndex++;
                     }
-                    sumResult += Integer.parseInt(s.substring(beginIndex, endIndex));
+                    if (hex) {
+                        sumResult += (int) Long.parseLong(s.substring(beginIndex, endIndex), 16);
+                    } else {
+                        sumResult += Integer.parseInt(s.substring(beginIndex, endIndex));
+
+                    }
                     beginIndex = endIndex;
                 } else {
                     beginIndex++;
