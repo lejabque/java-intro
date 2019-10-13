b86e5514e6719dd85312d9dc2b8983d31d8dd8be
lejabque <vorkdenis1@gmail.com>
2019-10-13 23:19:08 +0200

    HW6 complete (no mod, no review)

--- hw6/src/WordStatIndex.java
+++ hw6/src/WordStatIndex.java
@@ -5,44 +5,50 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-class Pair {
-    int count;
-    IntList list;
-
-    Pair(int newPos) {
-        this.count = 1;
-        this.list = new IntList(5);
-        list.add(newPos);
-    }
-}
-
 class IntList {
     private int[] list;
     private int listSize;
 
-    public IntList(int newLength) {
-        list = new int[newLength];
+    IntList(int x) {
+        list = new int[5];
         listSize = 0;
+        this.add(x);
     }
 
-    public void add(int x) {
+    void add(int x) {
         list[listSize++] = x;
         if (listSize == list.length) {
             list = Arrays.copyOf(list, listSize * 2);
         }
     }
+
+    String printList() {
+        StringBuilder res = new StringBuilder();
+        res.append(listSize);
+        for (int i = 0; i < listSize; ++i) {
+            res.append(" ").append(list[i]);
+        }
+        return res.toString();
+    }
 }
 
 public class WordStatIndex {
     public static void main(String[] args) {
-        Map<String, Pair> wordCounter = new LinkedHashMap<>();
+        Map<String, IntList> wordCounter = new LinkedHashMap<>();
         try (FastScanner in = new FastScanner(new File(args[0]), "utf-8")) {
+            int i = 1;
             while (in.hasNextWord()) {
-                String s = in.nextWord().toLowerCase();
-                wordCounter.merge(s, 1, Integer::sum);
+                String word = in.nextWord().toLowerCase();
+                IntList foundList = wordCounter.get(word);
+                if (foundList == null) {
+                    wordCounter.put(word, new IntList(i));
+                } else {
+                    foundList.add(i);
+                }
+                i++;
             }
         } catch (UnsupportedEncodingException e) {
-            System.err.println("Uncorrect encoding: " + e.getMessage());
+            System.err.println("Incorrect encoding: " + e.getMessage());
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
         } catch (IOException e) {
@@ -52,9 +58,8 @@ public class WordStatIndex {
         try (BufferedWriter out = new BufferedWriter(
                 new OutputStreamWriter(new FileOutputStream(args[1]), StandardCharsets.UTF_8))) {
             String[] keys = wordCounter.keySet().toArray(new String[0]);
-            Arrays.sort(keys);
             for (String key : keys) {
-                out.write(key + " " + wordCounter.get(key) + "\n");
+                out.write(key + " " + wordCounter.get(key).printList() + "\n");
             }
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
