99237300d8dc94467c867f0d47cc7e15b2660562
lejabque <vorkdenis1@gmail.com>
2019-11-11 14:51:51 +0300

    HW9 tests accepted

--- hw9/src/md2html/Md2Html.java
+++ hw9/src/md2html/Md2Html.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 
-public class md2html {
+public class md2Html {
 
     static class ParagraphConverter {
         private Map<String, String> anyTags = new HashMap<String, String>();
@@ -120,12 +120,15 @@ public class md2html {
     }
 
     public static void main(String[] args) {
-        String inFile = "/home/lejabque/Desktop/Study/term1/hw/prog-intro-hw/hw9/src/md2html/md.in";
-        // String inFile = args[0];
+//        String inFile = "/home/lejabque/Desktop/Study/term1/hw/prog-intro-hw/hw9/src/md2html/md.in";
+//        String outFile = "/home/lejabque/Desktop/Study/term1/hw/prog-intro-hw/hw9/src/md2html/md.out";
+        String inFile = args[0];
+        String outFile = args[1];
         ParagraphConverter converter = new ParagraphConverter();
         try (BufferedReader in = new BufferedReader(
                 new InputStreamReader(
-                        new FileInputStream(new File(inFile)), StandardCharsets.UTF_8))) {
+                        new FileInputStream(new File(inFile)), StandardCharsets.UTF_8));
+             PrintWriter out = new PrintWriter(new File(outFile))) {
             String line = in.readLine();
             while (line != null) {
                 StringBuilder paragraph = new StringBuilder();
@@ -142,13 +145,15 @@ public class md2html {
                 if (paragraph.length() > 0) {
                     StringBuilder resParagraph = new StringBuilder();
                     converter.convert(paragraph.toString(), resParagraph);
-                    System.out.println(resParagraph.toString());
+                    out.println(resParagraph.toString());
                 }
             }
         } catch (FileNotFoundException e) {
             System.err.println("File not found: " + e.getMessage());
+            return;
         } catch (IOException e) {
             System.err.println("I/O error: " + e.getMessage());
+            return;
         }
     }
 }
