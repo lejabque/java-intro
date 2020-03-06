1478bf8ccf3a0878fd4be24c10e5edf8755e2de6
lejabque <vorkdenis1@gmail.com>
2020-03-06 11:55:36 +0300

    HW16: contract fix

--- hw16_linked_queue/src/queue/Queue.java
+++ hw16_linked_queue/src/queue/Queue.java
@@ -6,11 +6,11 @@ public interface Queue {
 
     // Pre: element != null
     void enqueue(Object element);
-    // Post: n' = n + 1 && (i in [1..n-1] -> a[i]' = a[i]) && a[n] = element
+    // Post: n' = n + 1 && (i in [1..n] -> a[i]' = a[i]) && a[n+1]' = element
 
     // Pre: n > 0
     Object dequeue();
-    // Post: n' = n - 1 && (i in [1..n-1] -> a[i]' = a[i + 1]) && a[n] = null
+    // Post: n' = n - 1 && (i in [1..n-1] -> a[i]' = a[i + 1]) && a[n]' = null
     // R = a[1]
 
     // Pre: n > 0
