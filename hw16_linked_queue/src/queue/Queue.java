297e08373fe34418c78177e29ac7d86b28e2baf0
lejabque <vorkdenis1@gmail.com>
2020-03-13 02:35:00 +0300

    HW16: ifWhile contracts update

--- hw16_linked_queue/src/queue/Queue.java
+++ hw16_linked_queue/src/queue/Queue.java
@@ -26,24 +26,30 @@ public interface Queue {
     void clear();
 
     // Pre: pred != null
-    // Post: M: (m in M -> 1 <= m <= n && pred(a[m]) == false) && (i < j in [1..|M|] -> M[i] < M[j])
-    // && n' = |M| && (1 <= i <= n' -> a[i]' = a[M[i]])
+    //
+    // let M - set: (m in M -> 1 <= m <= n && pred(a[m]) == false) && (m not in M && 1 <= m <= n -> pred(a[m]) == true) &&
+    // (i < j in [1..|M|] -> M[i] < M[j])
+    // Post: n' = |M| && (1 <= i <= n' -> a[i]' = a[M[i]])
     void removeIf(Predicate<Object> pred);
 
 
     // Pre: pred != null
-    // Post: M: (m in M -> 1 <= m <= n && pred(a[m]) == true) && (i < j in [1..|M|] -> M[i] < M[j])
-    // && n' = |M| && (1 <= i <= n' -> a[i]' = a[M[i]])
+    //
+    // let M - set: (m in M -> 1 <= m <= n && pred(a[m]) == true) && (m not in M && 1 <= m <= n -> pred(a[m]) == false)
+    // && (i < j in [1..|M|] -> M[i] < M[j])
+    // Post: n' = |M| && (1 <= i <= n' -> a[i]' = a[M[i]])
     void retainIf(Predicate<Object> pred);
 
     // Pre: pred != null
-    // Post: 0 <= m <= n: (1 <= i <= m -> pred(a[i]) == true) && (m < n - > pred(a[m + 1] == false)
-    // && n' = m && (1 <= i <= n' -> a[i]' = a[i])
+    //
+    // let m: 0 <= m <= n && (1 <= i <= m -> pred(a[i]) == true) && (m < n - > pred(a[m + 1] == false)
+    // Post: n' = m && (1 <= i <= n' -> a[i]' = a[i])
     void takeWhile(Predicate<Object> pred);
 
     // Pre: pred != null
-    // Post: 0 <= m <= n: (1 <= i <= m -> pred(a[i]) == true) && (m < n - > pred(a[m + 1] == false)
-    // && n' = n - m && (1 <= i <= n' -> a[i]' = a[m + i])
+    //
+    // let m: 0 <= m <= n && (1 <= i <= m -> pred(a[i]) == true) && (m < n - > pred(a[m + 1] == false)
+    // Post: n' = n - m && (1 <= i <= n' -> a[i]' = a[m + i])
     void dropWhile(Predicate<Object> pred);
 
     // Post: R = (n == 0) && immutable
